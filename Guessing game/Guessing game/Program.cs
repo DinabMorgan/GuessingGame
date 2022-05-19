840aa961dc369258ce3f66cf31eb6572e7832eca
DinabMorgan <morgan.dinab@gmail.com>
2022-05-19 14:44:14 -0400

    part 1 of day 2

--- Guessing game/Guessing game/Program.cs
+++ Guessing game/Guessing game/Program.cs
@@ -2,3 +2,10 @@
 Console.WriteLine("Hello, Welcome to the guessing game!");
 // adding a variable 1-10
 string num = "5";
+Console.WriteLine("guess a number 1 -10");
+   String num2 =  Console.ReadLine();
+int num3 = Int32.Parse(num2);
+if (num ==num2)
+{
+    Console.WriteLine("great job, you guessed correctly!");
+}
