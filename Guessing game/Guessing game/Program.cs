ba87e69bb05ae67cb8b3dc0da50f7bb434371f16
MiguelRosado-WCCI <M.rosado1725@gmail.com>
2022-05-19 15:12:36 -0400

    steps 3-4 day 2

--- Guessing game/Guessing game/Program.cs
+++ Guessing game/Guessing game/Program.cs
@@ -1,11 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Welcome to the guessing game!");
 // adding a variable 1-10
-string num = "5";
+int num = 5;
 Console.WriteLine("guess a number 1 -10");
-   String num2 =  Console.ReadLine();
-int num3 = Int32.Parse(num2);
+   string input =  Console.ReadLine();
+int num2 = Int32.Parse(input);
 if (num ==num2)
 {
     Console.WriteLine("great job, you guessed correctly!");
+}
+else if (num2 >= 10) {
+    Console.WriteLine("You have the choice of 1 through 10");
+}
+else {
+    Console.WriteLine("You guessed wrong :(");
+
 }
