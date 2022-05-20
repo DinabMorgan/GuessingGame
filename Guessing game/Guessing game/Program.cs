73502642f299a0339ce8444fe67172e5e62b0203
DinabMorgan <morgan.dinab@gmail.com>
2022-05-20 14:36:40 -0400

    did all but the extra track

--- Guessing game/Guessing game/Program.cs
+++ Guessing game/Guessing game/Program.cs
@@ -2,17 +2,45 @@
 Console.WriteLine("Hello, Welcome to the guessing game!");
 // adding a variable 1-10
 int num = 5;
-Console.WriteLine("guess a number 1 -10");
-   string input =  Console.ReadLine();
-int num2 = Int32.Parse(input);
-if (num ==num2)
-{
-    Console.WriteLine("great job, you guessed correctly!");
-}
-else if (num2 >= 10) {
-    Console.WriteLine("You have the choice of 1 through 10");
-}
-else {
-    Console.WriteLine("You guessed wrong :(");
 
+
+
+
+
+
+
+/*for (int i = 0; i <= 3; i++)
+{
+    Console.WriteLine(" great guess! you are  " + (num - num2) + " far from the correct response.");
+
+} */
+int attempt = 1;
+while (attempt <= 3)
+{
+    Console.WriteLine("guess a number 1 -10");
+    string input = Console.ReadLine();
+
+    int num2 = Int32.Parse(input);
+
+   
+
+    if (num == num2)
+    {
+        Console.WriteLine("great job, you guessed correctly!");
+        attempt = 4;
+
+    }
+    else if (num2 >= 11 || num2 <= 0)
+    {
+        Console.WriteLine("You have the choice of 1 through 10");
+    }
+    else
+    {
+        Console.WriteLine("You guessed wrong :(");
+        Console.WriteLine(" great guess! you are  " + (num - num2) + " far from the correct response.");
+        attempt++;
+
+
+    }
 }
+
